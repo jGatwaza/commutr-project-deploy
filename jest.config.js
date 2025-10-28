53dcdbcfc5d24e625c60bd3a1c43d7acaec73735
ZainabAdamji <zadamji@college.harvard.edu>
2025-10-28 01:59:45 -0400

    chore(test): align Jest ESM config and scripts [CTR-30]

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,14 @@
 export default {
+  preset: 'ts-jest/presets/default-esm',
   testEnvironment: "node",
-  testMatch: ["**/tests/**/*.test.js"],
-  transform: {}
+  testMatch: ["**/tests/**/*.test.js", "**/__tests__/**/*.test.ts"],
+  extensionsToTreatAsEsm: ['.ts'],
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+  },
+  transform: {
+    '^.+\\.ts$': ['ts-jest', {
+      useESM: true,
+    }],
+  },
 };
