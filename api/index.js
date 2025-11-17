a65d1ccb0eae51ac41686d1788db05fcd2d4acb3
JEAN YVES GATWAZA <jgatwazakubwimana@college.harvard.edu>
2025-11-17 12:50:48 -0500

    Add explicit express import for Vercel detection

--- api/index.js
+++ api/index.js
@@ -1,5 +1,8 @@
 // Vercel serverless function entry point
+import express from 'express';
+
+// Import the Express app
 import app from '../src/server.js';
 
-// Export handler function for Vercel
+// Export the app as the default handler
 export default app;
